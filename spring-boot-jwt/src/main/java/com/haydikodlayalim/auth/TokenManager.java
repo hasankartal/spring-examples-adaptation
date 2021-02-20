442d4b1d1425ecc2921e94214bc460d49ead7512
Hasan Kartal <hasankartal18@gmail.com>
2021-02-20 22:10:21 +0300

    haydikodlayalim - json web token implementation

---
+++ spring-boot-jwt/src/main/java/com/haydikodlayalim/auth/TokenManager.java
@@ -0,0 +1,48 @@
+package com.haydikodlayalim.auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class TokenManager {
+
+    private static final int validity = 5 * 60 * 1000;
+    Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuer("www.pusat.com")
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + validity))
+                .signWith(key)
+                .compact();
+    }
+
+    public boolean tokenValidate(String token) {
+        if (getUserNameToken(token) != null && isExpired(token)) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getUserNameToken(String token) {
+        Claims claims = getClaims(token);
+        return claims.getSubject();
+    }
+
+    public boolean isExpired(String token) {
+        Claims claims = getClaims(token);
+        return claims.getExpiration().after(new Date(System.currentTimeMillis()));
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+    }
+}
